d334984c5bcaf90a40daf5899878b850931ebe7b
Diego-ds <54712482+Diego-ds@users.noreply.github.com>
2023-04-16 17:43:29 -0500

    added creator profile activity

---
+++ app/src/main/java/com/example/spooon_app/ProfileCreatorActivity.kt
@@ -0,0 +1,16 @@
+package com.example.spooon_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.spooon_app.databinding.ActivityProfileCreatorBinding
+import com.example.spooon_app.databinding.ActivityRecipeViewBinding
+
+class ProfileCreatorActivity : AppCompatActivity() {
+    private val binding: ActivityProfileCreatorBinding by lazy{
+        ActivityProfileCreatorBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
--- app/src/main/java/com/example/spooon_app/RecipeViewActivity.kt
+++ app/src/main/java/com/example/spooon_app/RecipeViewActivity.kt
@@ -1,7 +1,10 @@
 package com.example.spooon_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.spooon_app.databinding.ActivityCreateRecipeBinding
 import com.example.spooon_app.databinding.ActivityRecipeViewBinding
 
@@ -14,5 +17,15 @@ class RecipeViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult(),::onResult)
+        binding.creatorprofile.setOnClickListener {
+            val intent = Intent(this, ProfileCreatorActivity::class.java)
+            launcher.launch(intent)
+        }
     }
+
+    fun onResult(result:ActivityResult){
+
+    }
+
 }
